e0ffdd4bf1e4deab0bb665847edb2bf706018947
smha118 <smha118@gmail.com>
2024-01-04 13:26:41 -0800

    pharmomics debug

--- runpharmomics.php
+++ runpharmomics.php
@@ -153,11 +153,10 @@ debug_to_console("line152");
 $login_button = '';
 $scriptUri = "http://" . $_SERVER["HTTP_HOST"] . "/runpharmomics.php?fromapp2=true";
 debug_to_console($scriptUri);
-require_once("vendor/autoload.php");
+require_once("vendor/src/Google/autoload.php");
 $google_client== new Google_Client();
-debug_to_console($google_client);
 $google_client->setRedirectUri($scriptUri);
-debug_to_console($google_client);
+debug_to_console("line160");
 //This $_GET["code"] variable value received after user has login into their Google Account redirct to PHP script then this variable value has been received
 if (isset($_GET["code"])) {
   //It will Attempt to exchange a code for an valid authentication token.
