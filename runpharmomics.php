1199a43e789e67ac0588aaa15dd409b083eb33c9
smha118 <smha118@gmail.com>
2024-01-04 13:29:03 -0800

    pharmomics debug

--- runpharmomics.php
+++ runpharmomics.php
@@ -152,9 +152,9 @@ if (isset($_GET['sessionID']) ? $_GET['sessionID'] : null) {
 debug_to_console("line152");
 $login_button = '';
 $scriptUri = "http://" . $_SERVER["HTTP_HOST"] . "/runpharmomics.php?fromapp2=true";
-debug_to_console($scriptUri);
+#debug_to_console($scriptUri);
 require_once("vendor/src/Google/autoload.php");
-$google_client== new Google_Client();
+#$google_client== new Google_Client();
 $google_client->setRedirectUri($scriptUri);
 debug_to_console("line160");
 //This $_GET["code"] variable value received after user has login into their Google Account redirct to PHP script then this variable value has been received
