7d817a07262e7b0cef8df53b0ec34d987e5f026c
smha118 <smha118@gmail.com>
2024-01-08 11:43:06 -0800

    app1_debug

--- app1_parameters.php
+++ app1_parameters.php
@@ -209,7 +209,8 @@ body.loading .ajaxloading {
               <?php
               foreach ($drug_list as $item) {
                 #debug_to_console("$item");
-                echo "<option value = \"$item\">$item</option>";
+                #echo "<option value = \"$item\">$item</option>";
+                echo $item;
               }
 
               ?>
