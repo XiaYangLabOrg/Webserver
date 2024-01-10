69d6d8701d4beef4199eb078ee3dbfc2dbc03726
smha118 <smha118@gmail.com>
2024-01-10 13:33:20 -0800

    app2 debug

--- result_shinyapp2.php
+++ result_shinyapp2.php
@@ -85,7 +85,6 @@ if ($signature == 1) { //meta
     if ($run == 'T' && !file_exists($frunning_status)) {
       $outfile = $ROOT_DIR . "Data/Pipeline/Results/shinyapp2/" . $sessionID . "out.txt";
       shell_exec("touch " . $frunning_status);
-      debug_to_console($ROOT_DIR . "R-3.4.4/bin/Rscript " . $ROOT_DIR . "Data/Pipeline/" . $sessionID . "app2.R | tee " . $outfile);
       shell_exec("Rscript " . $ROOT_DIR . "Data/Pipeline/" . $sessionID . "app2.R | tee " . $outfile);
       #shell_exec("sh run_app2.sh $sessionID | tee " . $outfile);
       sleep(1);
@@ -135,48 +134,48 @@ if ($signature == 1) { //meta
   $google_client->setRedirectUri($scriptUri);
 
   //This $_GET["code"] variable value received after user has login into their Google Account redirct to PHP script then this variable value has been received
-  if (isset($_GET["code"])) {
-    //It will Attempt to exchange a code for an valid authentication token.
-    #    $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
-    $google_client->authenticate($_GET['code']);
-    $_SESSION['access_token'] = $google_client->getAccessToken();
+  // if (isset($_GET["code"])) {
+  //   //It will Attempt to exchange a code for an valid authentication token.
+  //   #    $token = $google_client->fetchAccessTokenWithAuthCode($_GET["code"]);
+  //   $google_client->authenticate($_GET['code']);
+  //   $_SESSION['access_token'] = $google_client->getAccessToken();
 
-    //This condition will check there is any error occur during geting authentication token. If there is no any error occur then it will execute if block of code/
-    if (!isset($token['error'])) {
-      //Set the access token used for requests
-      $google_client->setAccessToken($token['access_token']);
+  //   //This condition will check there is any error occur during geting authentication token. If there is no any error occur then it will execute if block of code/
+  //   if (!isset($token['error'])) {
+  //     //Set the access token used for requests
+  //     $google_client->setAccessToken($token['access_token']);
 
-      //Store "access_token" value in $_SESSION variable for future use.
-      $_SESSION['access_token'] = $token['access_token'];
+  //     //Store "access_token" value in $_SESSION variable for future use.
+  //     $_SESSION['access_token'] = $token['access_token'];
 
-      //Create Object of Google Service OAuth 2 class
-      $google_service = new Google_Service_Oauth2($google_client);
+  //     //Create Object of Google Service OAuth 2 class
+  //     $google_service = new Google_Service_Oauth2($google_client);
 
-      //Get user profile data from google
-      $data = $google_service->userinfo->get();
-      //debug_to_console($data);
-      //Below you can find Get profile data and store into $_SESSION variable
-      if (!empty($data['given_name'])) {
-        $_SESSION['user_first_name'] = $data['given_name'];
-      }
+  //     //Get user profile data from google
+  //     $data = $google_service->userinfo->get();
+  //     //debug_to_console($data);
+  //     //Below you can find Get profile data and store into $_SESSION variable
+  //     if (!empty($data['given_name'])) {
+  //       $_SESSION['user_first_name'] = $data['given_name'];
+  //     }
 
-      if (!empty($data['family_name'])) {
-        $_SESSION['user_last_name'] = $data['family_name'];
-      }
+  //     if (!empty($data['family_name'])) {
+  //       $_SESSION['user_last_name'] = $data['family_name'];
+  //     }
 
-      if (!empty($data['email'])) {
-        $_SESSION['user_email_address'] = $data['email'];
-      }
+  //     if (!empty($data['email'])) {
+  //       $_SESSION['user_email_address'] = $data['email'];
+  //     }
 
-      if (!empty($data['gender'])) {
-        $_SESSION['user_gender'] = $data['gender'];
-      }
+  //     if (!empty($data['gender'])) {
+  //       $_SESSION['user_gender'] = $data['gender'];
+  //     }
 
-      if (!empty($data['picture'])) {
-        $_SESSION['user_image'] = $data['picture'];
-      }
-    }
-  }
+  //     if (!empty($data['picture'])) {
+  //       $_SESSION['user_image'] = $data['picture'];
+  //     }
+  //   }
+  // }
   #$user_name = $_SESSION['user_first_name'] . " " . $_SESSION['user_last_name'];
   #$user_email = $_SESSION['user_email_address'];
   // run commands on hoffman2
