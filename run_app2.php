20302be71948acbf761237f24561fa762a30a766
smha118 <smha118@gmail.com>
2024-02-13 12:37:33 -0800

    test added

--- run_app2.php
+++ run_app2.php
@@ -2,6 +2,10 @@
 include "functions.php";
 $ROOT_DIR = $_SERVER['DOCUMENT_ROOT'] . "/";
 $env=parse_ini_file(".env");
+$connection = ssh2_connect($env["HOFFMAN2_SERVER_IP"], 22);
+ssh2_auth_password($connection, $env["PHARMOMICS_USERNAME"], $env["PHMARMOMICS_PASSWORD"]);
+$fpathOut="/var/www/mergeomics/html/Data/Pipeline/7DEB5jWjP2_app2_seg.R";
+ssh2_scp_send($connection, $fpathOut, '/u/scratch/m/mergeome/app2seg/7DEB5jWjP2_app2_seg.R', 0644);
 if (isset($_POST['sessionID'])) {
     $sessionID = $_POST['sessionID'];
 }
