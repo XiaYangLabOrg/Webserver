624fcd86babffee5d0ceeba22dccd047d09ca4a1
smha118 <smha118@gmail.com>
2024-02-14 10:01:39 -0800

    test ssh2_exec

--- test.php
+++ test.php
@@ -8,6 +8,9 @@ echo $env["HOFFMAN2_SERVER_IP"];
 $connection = ssh2_connect($env["HOFFMAN2_SERVER_IP"], 22);
 ssh2_auth_password($connection, $env["PHARMOMICS_USERNAME"], $env["PHMARMOMICS_PASSWORD"]);
 $fpathOut="/var/www/mergeomics/html/Data/Pipeline/7DEB5jWjP2_app2_seg.R";
-ssh2_scp_send($connection, $fpathOut, '/u/scratch/m/mergeome/app2seg/7DEB5jWjP2_app2_seg.R', 0644);
+#sah2_scp_send($connection, $fpathOut, '/u/scratch/m/mergeome/app2seg/7DEB5jWjP2_app2_seg.R', 0644);
+$stream=ssh2_exec($connection, "ls");
+fclose($stream);
+
 
 ?>
