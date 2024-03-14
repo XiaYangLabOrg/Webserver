e3e256430b7d3d9d30c80cafdca03b91dc5718b2
smha118 <smha118@gmail.com>
2024-03-14 15:51:10 -0700

    debug meta

--- META_buttons.php
+++ META_buttons.php
@@ -77,6 +77,7 @@ if (!file_exists($fsession) || $rollback == 'T') {
 </div>
 <script src="include/js/jquery.js"></script>
 <script type="text/javascript">
+	console.log("<?php echo $fsession;?>");
 	var meta_string = "<?php echo $meta_sessionID; ?>";
 	var n = localStorage.getItem('on_load_session');
 	localStorage.setItem("on_load_session", meta_string);
