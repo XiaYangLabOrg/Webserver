cb28e812773397a5e939df42076a9288505dd317
smha118 <smha118@gmail.com>
2024-05-03 00:17:53 -0700

    kda loading

--- cyto_visualize/loading.php
+++ cyto_visualize/loading.php
@@ -49,6 +49,17 @@
 
     <script type="text/javascript">
         var sessionID="<?php echo $sessionID;?>";
+        $.ajax({
+            url: "/cyto_visualize/write_cytoscape.php",
+            global: false, type: "GET", 
+            timeout: 1000 * 60 * 20,
+            data: ({"sessionID":sessionID}), 
+            cache: false,
+            success: function(html) {
+                console.log(html);
+                window.location = html;
+            }
+            });
         setTimeout(function() {
             $.ajax({
             url: "/cyto_visualize/write_cytoscape.php",
